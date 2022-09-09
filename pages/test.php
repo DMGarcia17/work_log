b5a608881303efff7412ea710e84529382e254f5
Daniel Moreno <daniel.moreno@creativaconsultores.com>
2022-09-09 00:58:49 -0600

    Displayed name for extension

--- pages/test.php
+++ pages/test.php
@@ -2,7 +2,7 @@
     require_once '../core/Connection.php';
     $db = new DatabaseConnection();
 
-    $res = $db->blankect_query('files', '*');
+    $res = $db->blankect_query('files a', 'a.ID, a.name, (select b.extension from file_extensions b where b.ID = a.extension) extension');
     $formated = array('data' => $res);
     //$arr = array('a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5);
     echo json_encode($formated);
